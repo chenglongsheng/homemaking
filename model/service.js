a281804157acc18a428a1b301746b4037faeac68
成龙生 <1536463948@qq.com>
2022-09-26 09:34:06 +0800

    上拉触底刷新接口

--- model/service.js
+++ model/service.js
@@ -4,22 +4,39 @@
 import Http from "../utils/http";
 
 class Service {
+
+    page = 1
+    size = 4
+    data = []
+    hasMoreData = true
+
     /**
      * 分页获取服务列表
-     * @param page          页码
-     * @param size          每页数量
      * @param category_id   分类id
      * @param type          服务类型
      */
-    async getServiceList(page, size, category_id = null, type = null) {
+    async getServiceList(category_id = null, type = null) {
+
+        // 避免一直请求
+        if (!this.hasMoreData) {
+            return this.data
+        }
+
         console.log("获取服务列表")
-        return Http.request({
+        const serviceList = await Http.request({
             url: 'v1/service/list',
             data: {
-                page,
-                size
+                page: this.page,
+                size: this.size,
+                category_id: category_id || '',
+                type: type || ''
             }
         })
+        // 每次下拉合并数据
+        this.data = this.data.concat(serviceList.data)
+        this.hasMoreData = !(this.page === serviceList.last_page)
+        this.page++
+        return this.data
     }
 }
 
--- pages/home/home.js
+++ pages/home/home.js
@@ -77,11 +77,55 @@ Page({
             score: "0",
             title: "北京路找一名保安",
             type: 2
+        }, {
+            category: {
+                id: 6, name: "护理"
+            },
+            cover_image: {
+                id: 24, path: "https://note-1010.oss-cn-beijing.aliyuncs.com/img/image-20210829230656830.png"
+            },
+            create_time: "2022-09-25 14:02:03",
+            description: "家中有100岁男性老人XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            designated_place: 0,
+            id: 19,
+            price: "12000.0",
+            publisher: {
+                id: 1,
+                nickname: "XXX",
+                avatar: "https://note-1010.oss-cn-beijing.aliyuncs.com/img/boy.jpg",
+                real_name: "xinmo",
+                gender: 1
+            },
+            sales_volume: 0,
+            score: "0",
+            title: "北京路找一名保安",
+            type: 2
         }]
     },
+    /**
+     * 页面加载
+     * @param options
+     */
     onLoad: function (options) {
         // this._getServiceList()
         // this._getCategoryList()
+    },
+    /**
+     * 下拉刷新
+     */
+    onPullDownRefresh() {
+        console.log("下拉刷新")
+    },
+    /**
+     * 上拉触底
+     */
+    async onReachBottom() {
+        console.log("上拉触底")
+        if (!service.hasMoreData) {
+            return
+        }
+        const serviceList = await service.getServiceList()
+        this.setData({serviceList})
     },
     async _getServiceList() {
         await service.getServiceList(1, 10)
