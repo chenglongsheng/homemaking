3f31ef3e405b131ad53d68bffeff8c6770cee895
成龙生 <1536463948@qq.com>
2022-09-26 11:27:05 +0800

    状态展示组件

---
+++ components/show-status/show-status.js
@@ -0,0 +1,17 @@
+Component({
+    properties: {
+        show: {
+            type: Boolean,
+            value: false
+        },
+        content: {
+            type: String
+        },
+        top: {
+            type: String,
+            value: '0'
+        }
+    },
+    data: {},
+    methods: {}
+});
