1d1ca349db3798fc9f89d19656e932579d5cdc6e
成龙生 <1536463948@qq.com>
2022-09-27 18:30:43 +0800

    自定义头像组件和转换函数

--- pages/service-detail/service-detail.js
+++ pages/service-detail/service-detail.js
@@ -1,14 +1,29 @@
 import Service from "../../model/service";
+import User from "../../model/User";
 
 Page({
     data: {
-        service: null,
-        serviceId: null
+        service: {
+            id: 10,
+            type: 2,
+            designated_place: 0,
+            title: '北京招工',
+            description: "adasdasafiuasdhfasufh",
+            price: '123.00',
+            begin_date: "2021-04-29",
+            score: 0,
+            status: 2,
+            sales_volume: 0,
+            create_time: '2021-04-29 20:45:23'
+        },
+        serviceId: null,
+        isPublisher: false
     },
-    onLoad: function (options) {
+    onLoad: async function (options) {
         console.log(options)
         this.data.serviceId = options.service_id
-        // this._getService()
+        // await this._getService()
+        this._checkRole()
     },
 
     async _getService() {
@@ -16,6 +31,15 @@ Page({
         this.setData({
             service
         })
+    },
+
+    _checkRole() {
+        const userinfo = User.getUserInfoByLocal()
+        if (userinfo && userinfo.id === this.data.service.publisher.id) {
+            this.setData({
+                isPublisher: true
+            })
+        }
     }
 
 });
